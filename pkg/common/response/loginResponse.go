bca6ac495cf0b490cf2efda8b0e1a1e9d35b4c35
DemonKHH <597406711@qq.com>
2023-04-10 11:22:48 +0800

    add login logic

--- pkg/common/response/loginResponse.go
+++ pkg/common/response/loginResponse.go
@@ -6,7 +6,7 @@ type LoginResponse struct {
 	Name         *string   `json:"name" validate:"required,min=2,max=100"`
 	Email        *string   `json:"email" validate:"email,required"`
 	Avator       *string   `json:"avator"`
-	Mac          *string   `json:"mac"`
+	Mac          *string   `json:"mac,omitempty"`
 	AccessToken  *string   `json:"accessToken"`
 	RefreshToken *string   `json:"refreshToken"`
 	CreatedAt    time.Time `json:"createdAt"`
