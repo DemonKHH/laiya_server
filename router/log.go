53d60b83b772f1d146c336ed4e8f30f725e9c515
DemonKHH <597406711@qq.com>
2023-05-04 21:58:30 +0800

    新增跨域支持

--- router/log.go
+++ router/log.go
@@ -2,10 +2,11 @@ package router
 
 import (
 	"laiya_server/controllers"
+	"laiya_server/middleware"
 
 	"github.com/gin-gonic/gin"
 )
 
 func Log(r *gin.Engine) {
-	r.POST("/log/upload", controllers.UploadLog())
+	r.POST("/log/upload", middleware.Cors(), controllers.UploadLog())
 }
